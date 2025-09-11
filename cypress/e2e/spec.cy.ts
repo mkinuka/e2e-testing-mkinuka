c579ea5461fd8a1e36264d55dfd09220bff7ae87
moses kinuka <kinuka__@hotmail.comgit config --global user.email>
2025-09-11 21:42:27 +0200

    added tests for sorting

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,32 +1,76 @@
+import { should } from "chai";
+
+beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
 describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:5173/");
-  });
 
-  it("should have a search button", () => {
-    cy.visit("http://localhost:5173/");
+  it("should find html", () => {
     cy.get("button").should("contain.text", "Sök");
+    cy.get("#searchText").should("exist")
   });
 
-  it("should fetch movies containg (star)", () => {
-    cy.visit("http://localhost:5173/");
-
+  it("should fetch 10 movies containg (star)", () => {
     cy.get("input").type("star").should("have.value", "star");
-
     const knappen = cy.get("#search").should("exist");
-
     knappen.click();
+    cy.get("#movie-container .movie", { timeout: 5000 }).should("have.length", 10);
   });
 
-  it("should show fault message", () => {
-    cy.visit("http://localhost:5173/");
-
-    // cy.get("input").type("empty").should("have.value", "");
-
+  it("should display fault message", () => {
     const knappen = cy.get("#search").should("exist");
-
     knappen.click();
-
     cy.get("p").should("contain.text", "Inga sökresultat att visa");
   });
+
+  it("should show mocked data", () => {
+    cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {
+      statusCode: 200,
+      body: {
+        Search: [
+          { Title: "Harry Potter", Poster: "img url1" },
+          { Title: "Harry Potter 2", Poster: "img url2" }
+        ]
+      }
+    })
+    cy.get("#searchText").type("ABC{enter}")
+    cy.get("#movie-container .movie").should("have.length", 2);
+  })
+
+  it("Should return Sorted data from A-Ö", () => {
+  cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {
+    statusCode: 200,
+    body: {
+      Search: [
+        { Title: "Bertils Film", Poster: "img url1" },
+        { Title: "Christers Film", Poster: "img url3" },
+        { Title: "Alberts Film", Poster: "img url2" }
+      ]
+    }
+  })
+  cy.get("#searchText").type("WASD{enter}")
+  cy.get("#movie-container .movie").should("have.length", 3);
+
+  cy.get("#sort").click()
+  cy.get("#movie-container .movie").first().should("contain.text", "Alberts Film")
+})
+
+  it("Should return Sorted data from Ö-A", () => {
+  cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {
+      statusCode: 200,
+      body: {
+        Search: [
+          { Title: "Bertils Film", Poster: "img url1" },
+          { Title: "Christers Film", Poster: "img url3" },
+          { Title: "Alberts Film", Poster: "img url2" }
+        ]
+      }
+    })
+    cy.get("#searchText").type("WASD{enter}")
+    cy.get("#movie-container .movie").should("have.length", 3);
+    
+    cy.get("#sortInc").click()
+    cy.get("#movie-container .movie").first().should("contain.text", "Christers Film")
+  })
+
 });
